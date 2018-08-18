e0a253433e495a817a00a05f2d2eb557b31f13dc
ddarylmae <diieemmobee@gmail.com>
2018-08-19 11:06:37 +1200

    getTripsByRouteId working

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ var options = {
   };
 
 app.get('/getTripStatus', function () {
-    getRouteId('NEX');
+    getRouteId('879');
 });
 
 // cron.schedule('1 * * * *', function() {
@@ -30,17 +30,81 @@ function getRouteId(routeShortName) {
           chunks.push(chunk);
         });      
         res.on("end", function () {
+            let routeIds = [];
             var body = JSON.parse(chunks);
-            getTripsByRouteId(body.response[0].route_id);
+            body.response.forEach(element => {
+                routeIds.push(element.route_id);
+                // console.log(element.route_id);
+            });
+            // getTripsByRouteId(body.response[0].route_id);
+            getTripsByRouteId(routeIds);
+
         });
     });
     req.end();
 }
 
-function getTripsByRouteId(routeId) {
-    console.log('routeId: ' + routeId);
-    options.path = "/v2/gtfs/trips/routeid/" + routeId;
-    var req = https.request(options, function (res) {
+// function getTripsByRouteId(routeId) {
+//     console.log('routeId: ' + routeId);
+//     options.path = "/v2/gtfs/trips/routeid/" + routeId;
+//     var req = https.request(options, function (res) {
+//         var chunks = [];
+//         res.on("data", function (chunk) {
+//           chunks.push(chunk);
+//         });      
+//         res.on("end", function () {
+//             var body = JSON.parse(chunks);
+//             let trips = [];
+//             body.response.forEach(element => {
+//                 trips.push(element.trip_id);
+//             });
+//             getTripStatus(trips);
+//         });
+//     });
+//     req.end();
+// }
+
+function getTripsByRouteId(routeIds) {
+    // console.log('routeId: ' + routeId);
+    let trips = [];
+    routeIds.forEach(rId => {
+        options.path = '/v2/gtfs/trips/routeid/' + rId;
+        console.log('Each route: ' + options.path);
+        let req = https.request(options, function (res) {
+            let chunks = [];
+            res.on("data", function (chunk) {
+                chunks.push(chunk);
+            });      
+            // console.log(res.data);
+            res.on("end", function () { 
+                let body = JSON.parse(chunks);
+                // console.log(body)
+                body.response.forEach(element => {
+                    // console.log(element);
+                    trips.push(element.trip_id);
+                    console.log(element.trip_id);
+                });
+            });
+        });
+        req.end();
+        console.log("ended")
+    });
+    console.log("executed")
+   // testFunc(trips);
+}
+
+function testFunc(trips) {
+    console.log('in test');
+    trips.forEach(element => {
+        console.log(element);
+    });
+}
+
+function getTripStatus(trips) {
+    trips.forEach(element => {
+        // console.log(element);
+        options.path = "/v2/gtfs/trips/routeid/" + routeId;
+        var req = https.request(options, function (res) {
         var chunks = [];
         res.on("data", function (chunk) {
           chunks.push(chunk);
@@ -55,11 +119,6 @@ function getTripsByRouteId(routeId) {
         });
     });
     req.end();
-}
-
-function getTripStatus(trips) {
-    trips.forEach(element => {
-        console.log(element);
     });
 }
 
